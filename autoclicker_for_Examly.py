2399ea0776e84c3e527a79f3c90b5249fd438251
phAni176 <phanitallapudi@gmail.com>
2022-08-11 23:46:25 +0530

    done with custom and examly input, adding backup file for certain line of code

--- autoclicker_for_Examly.py
+++ autoclicker_for_Examly.py
@@ -28,21 +28,27 @@ root=tk.Tk()
 root.geometry("600x400") 
 name_var=tk.StringVar()
 passw_var=tk.StringVar()
+
 def Close():
     root.destroy()
-var_for_custom_or_inbuilt = None 
+
 def submit():
     
     name=name_var.get()
     password=passw_var.get()
+    #####################################
         
-    #print("The name is : " + name)
-    print(password)
-    var_for_custom_or_inbuilt = password
+    #print(password)
+    #####################################
         
     name_var.set("")
     passw_var.set("")
     root.destroy()
+    
+    if password == 'no' or password == 'NO' or password == 'No':
+        print("passed")
+    else:
+        pass
 
 name_label = tk.Label(root, text = 'press yes for examly settings and no for custom', font=('calibre',10, 'bold'))
     
@@ -59,26 +65,10 @@ passw_entry.grid(row=2,column=1)
 sub_btn.grid(row=3,column=1)
 root.mainloop()
 
-print(var_for_custom_or_inbuilt)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 root = Tk()
 root.geometry("1280x540")
-def Close():
-    root.destroy()
 
 text = Label(text="To start the script press ready and then the countdown for 3 seconds start for the destination of the first click, however the mouse pointer over there!!!")
 text.place(x=70,y=90)
@@ -86,8 +76,10 @@ exit_button = Button(root, text="Ready", command=Close)
 exit_button.pack(padx=60,pady=20)
     
 root.mainloop()
-time.sleep(2)
+time.sleep(5)
 dest_x,dest_y = pyautogui.position()
+    #####################################
+
 print(dest_x,dest_y)
 print("Phase 1 done!!!")
     #####################################
@@ -96,15 +88,18 @@ root.geometry("1280x540")
 def Close():
     root.destroy()
 
-text = Label(text="Now we need to take input for the mouse pointer to comeback, press ready to")
+text = Label(text="Now we need to take input for the mouse pointer to comeback, press ready to start and counter will be for 5 seconds")
 text.place(x=70,y=90)
 exit_button = Button(root, text="Ready", command=Close)
 exit_button.pack(padx=60,pady=20)
     
 root.mainloop()
-time.sleep(2)
-dest_x,dest_y = pyautogui.position()
-print(dest_x,dest_y)
+time.sleep(5)
+
+end_x,end_y = pyautogui.position()
+print(end_x, end_y)
+
+
 def is_clicked(x, y, button, pressed):
     if pressed:
         print("clicked!!")
